014a48a086060158b975e0d7bc27196bbacfcaae
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-13 04:36:12 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Type of sum.js
@@ -0,0 +1,8 @@
+// -----------------------------------------------------------
+// Return the type of the sum of the two arguments
+// -----------------------------------------------------------
+
+function typeOfSum(a, b) {
+  var answer = a + b;
+  return typeof(answer);
+}
