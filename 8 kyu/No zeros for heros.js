32391ffd55f71e8e4396103b27b29f0cd98fcc9c
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-14 16:09:06 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/No zeros for heros.js
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------
+// Numbers ending with zeros are boring.
+// 
+// They might be fun in your world, but not here.
+// 
+// Get rid of them. Only the ending ones.
+// 
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// 
+// Zero alone is fine, don't worry about it. Poor guy anyway
+// -----------------------------------------------------------
+
+function noBoringZeros(n) {
+  if (n == 0){return n};
+  while (n % 10 == 0){
+    n = n / 10;
+  }
+  return n;
+}
