34aaa95b3414fa762ff6ef7ac5523fe3281e2a2f
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-18 01:03:44 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Switch it Up!.js
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------
+// When provided with a number between 0-9, return it in words.
+// 
+// Input :: 1
+// 
+// Output :: "One".
+// 
+// If your language supports it, try using a switch statement.
+// -----------------------------------------------------------
+
+function switchItUp(number){
+  var words = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"];
+  return words[number];
+}
