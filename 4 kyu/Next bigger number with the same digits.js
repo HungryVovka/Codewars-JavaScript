97a7b31976a12e0da8ff44456525c6bf5fb944f4
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-08-24 01:05:57 +0500

    feat: 4 kyu solutions

---
+++ 4 kyu/Next bigger number with the same digits.js
@@ -0,0 +1,42 @@
+// -----------------------------------------------------------
+// Create a function that takes a positive integer and returns the next bigger number that can be formed by rearranging its digits. For example:
+// 
+// 12 ==> 21
+// 513 ==> 531
+// 2017 ==> 2071
+// 
+// nextBigger(num: 12)   // returns 21
+// nextBigger(num: 513)  // returns 531
+// nextBigger(num: 2017) // returns 2071
+// 
+// If the digits can't be rearranged to form a bigger number, return -1 (or nil in Swift):
+// 
+// 9 ==> -1
+// 111 ==> -1
+// 531 ==> -1
+// 
+// nextBigger(num: 9)   // returns nil
+// nextBigger(num: 111) // returns nil
+// nextBigger(num: 531) // returns nil
+// -----------------------------------------------------------
+
+function nextBigger(n){
+  var bigger = -1;
+  var digits = n.toString().split("");
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < digits[i + 1]){
+      var nextdig = digits.splice(i + 1).sort();
+      for (let j = 0; j < nextdig.length; j++){
+        if (nextdig[j] > digits[i]){
+          digits[i] = nextdig[j] - digits[i];
+          nextdig[j] -= digits[i];
+          digits[i] += nextdig[j];
+          bigger = Number(digits.concat(nextdig).join(""), 10);
+          i = 0;
+          break;
+        }
+      }
+    }
+  }
+  return bigger;
+}
