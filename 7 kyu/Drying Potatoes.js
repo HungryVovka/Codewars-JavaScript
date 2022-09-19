57071b65cca18a7e6dda158e86d60624f8bd5a8f
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-09-19 14:11:54 +0500

    feat: 7 kyu solutions

--- 7 kyu/Drying Potatoes.js
+++ 7 kyu/Drying Potatoes.js
@@ -20,6 +20,7 @@
 // int parameter p0 - initial percent of water-
 // int parameter w0 - initial weight -
 // int parameter p1 - final percent of water -
+// 
 // potatoesshould return the final weight coming out of the oven w1 truncated as an int.
 // 
 // Example:
