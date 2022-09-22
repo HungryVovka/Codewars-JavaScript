2bca895cb690a55650e14ef89c61436f3f389ca8
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-09-22 14:38:22 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Formatting decimal places #0.js
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only 
+// valid numbers are used in the tests.
+// 
+// Example:    
+// 5.5589 is rounded 5.56   
+// 3.3424 is rounded 3.34
+// -----------------------------------------------------------
+
+function twoDecimalPlaces(n) {
+  return Math.round(n * 100) / 100;
+}
+
+// or
+
+function twoDecimalPlaces(n) {
+  return parseFloat(n.toFixed(2));
+}
+
+// or
+
+function twoDecimalPlaces(n) {
+  return +n.toFixed(2);
+}
---
+++ 8 kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------
+// Combine strings function
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+// 
+// Example:
+// 
+// combineNames('James', 'Stevens')
+// 
+// returns:
+// 
+// 'James Stevens'
+// -----------------------------------------------------------
+
+const combineNames = (first, second) => first + " " + second;
+
+// or
+
+const combineNames = (...args) => args.join(" ");
