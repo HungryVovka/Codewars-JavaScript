13d26c1de718d3e2361c23eb085db9b11e310a77
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-09-23 00:33:01 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Ensure question.js
@@ -0,0 +1,31 @@
+// -----------------------------------------------------------
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question 
+// mark, in which case, returns the original string.
+// 
+// For example (Input --> Output)
+// 
+// "Yes" --> "Yes?" 
+// "No?" --> "No?"
+// -----------------------------------------------------------
+
+function ensureQuestion(s) {
+  return s.split("?").join("").concat("?");
+}
+
+// or
+
+function ensureQuestion(s) {
+  return /\?$/.test(s) ? s : s + "?";
+}
+
+// or
+
+function ensureQuestion(s) {
+  return s.includes("?") ? s : `${s}?`;
+}
+
+// or
+
+function ensureQuestion(s) {
+  return s.endsWith("?") ? s : s + "?";
+}
