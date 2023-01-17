a19f98bbaeb894234d76d5c2c604055efee49c1b
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-01-17 19:08:51 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Count the divisors of a number.js
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------
+// Count the number of divisors of a positive integer n.
+// 
+// Random tests go up to n = 500000.
+// 
+// Examples (input --> output)
+// 4 --> 3 (1, 2, 4)
+// 5 --> 2 (1, 5)
+// 12 --> 6 (1, 2, 3, 4, 6, 12)
+// 30 --> 8 (1, 2, 3, 5, 6, 10, 15, 30)
+// 
+// Note you should only return a number, the count of divisors. The numbers between parentheses are shown only for you to see which numbers are 
+// counted in each case.
+// -----------------------------------------------------------
+
+function getDivisorsCnt(n){
+  let answer = 1;
+  for (let i = 0; i <= n / 2; i++){
+    if (n % i == 0){
+      answer += 1;
+    }
+  }
+  return answer;
+}
