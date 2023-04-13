c841eba1def5dc2787a3704fdb452f6cfdf5734d
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-04-14 00:27:13 +0500

    feat: 7 kyu solutions

---
+++ 7 kyu/Digit Digit.js
@@ -0,0 +1,37 @@
+// -----------------------------------------------------------
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+// 
+// For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1. (81-1-1-81)
+// 
+// Example #2: An input of 765 will/should return 493625 because 7^2 is 49, 6^2 is 36, and 5^2 is 25. (49-36-25)
+// 
+// Note: The function accepts an integer and returns an integer.
+// 
+// Happy Coding!
+// -----------------------------------------------------------
+
+function squareDigits(num){
+  var numbers = num.toString().split('').map(n => n * n).join('');
+  return parseInt(numbers);
+}
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2022 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-JavaScript
+// (https://github.com/HungryVovka/Codewars-JavaScript)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-JavaScript/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
