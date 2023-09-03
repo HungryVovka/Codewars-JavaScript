2727dec18bec7b7ec1e9a3b89b4f446dfa18fb32
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-09-03 23:30:08 +0500

    feat: 8 kyu solutions

---
+++ 8 kyu/Up and down, the string grows.js
@@ -0,0 +1,137 @@
+// -----------------------------------------------------------
+// Many programming languages provide the functionality of converting a string to uppercase or lowercase. For example, upcase/downcase in Ruby, 
+// upper/lower in Python, and toUpperCase/toLowerCase in Java/JavaScript. Typically, these methods won't change the size of the string.
+// 
+// For example, in Ruby, str.upcase.downcase.size == str.size is true for most cases.
+// 
+// However, in some special cases, the length of the transformed string may be longer than the original. Can you find a string that satisfies this criteria?
+// 
+// For example, in Ruby, That means str.upcase.downcase.size > str.size
+// 
+// You should just set the value of STRANGE_STRING to meet the previous criteria.
+// 
+// Note: Meta programming is not allowed in this kata. So, the size of your solution is limited.
+// -----------------------------------------------------------
+
+const STRANGE_STRING="\u1FE2";
+
+// or
+
+const STRANGE_STRING="\u1FE3";
+
+// or
+
+const STRANGE_STRING="\u1FB7";
+
+// or
+
+const STRANGE_STRING="\u1FB4";
+
+// or
+
+const STRANGE_STRING="\u1FB3";
+
+// or
+
+const STRANGE_STRING="\u1FB2";
+
+// or
+
+const STRANGE_STRING="\u1F97";
+
+// or
+
+const STRANGE_STRING="\uFB03";
+
+// or
+
+const STRANGE_STRING="\u0587";
+
+// or
+
+const STRANGE_STRING="\u1FF2";
+
+// or
+
+const STRANGE_STRING="\u0130";
+
+// or
+
+const STRANGE_STRING="\u00DF";
+
+// or
+
+const STRANGE_STRING="\u1E98";
+
+// or
+
+const STRANGE_STRING = "ß";
+
+// or
+
+const STRANGE_STRING = "İ";
+
+// or
+
+const STRANGE_STRING = "ẘ";
+
+// or
+
+const STRANGE_STRING = "ῲ";
+
+// or
+
+const STRANGE_STRING = "և";
+
+// or
+
+const STRANGE_STRING = "ῼ";
+
+// or
+
+const STRANGE_STRING = "ᾲ";
+
+// or
+
+const STRANGE_STRING = "ᾴ";
+
+// or
+
+const STRANGE_STRING = "ᾳ";
+
+// or
+
+const STRANGE_STRING = "ᾷ";
+
+// or
+
+const STRANGE_STRING = "ῗ";
+
+// or
+
+const STRANGE_STRING = "ῢ";
+
+// or
+
+const STRANGE_STRING = "ῧ";
+
+// -----------------------------------------------------------
+// License
+// Tasks are the property of Codewars (https://www.codewars.com/) 
+// and users of this resource.
+// 
+// All solution code in this repository 
+// is the personal property of Vladimir Rukavishnikov
+// (vladimirrukavishnikovmail@gmail.com).
+// 
+// Copyright (C) 2022 Vladimir Rukavishnikov
+// 
+// This file is part of the HungryVovka/Codewars-JavaScript
+// (https://github.com/HungryVovka/Codewars-JavaScript)
+// 
+// License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-JavaScript/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
+// -----------------------------------------------------------
